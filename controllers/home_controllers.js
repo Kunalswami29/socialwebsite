a6e579d0508441a2a076e764aaf824c14bea1b02
kunal swami <kunalswami29@gmail.com>
2020-05-22 18:20:48 +0530

    partial and layouts are added

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,5 +1,7 @@
 module.exports.home=function(req,res){
-    return res.end('<h1>Express is up!! </h1>')
+    return res.render('home',{
+        title:"HOME"
+    })
 };
 // module.exports.profile=function(req,res){
 //     return res.end('<h1>So what you wanna know about me ?? </h1>')
