fd18dd258a2c6c97a5bec394dbf3e5a39e5895f5
kunal swami <kunalswami29@gmail.com>
2020-05-23 18:05:02 +0530

    the separate style for pages is linked

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 const port=8000;
+//to use express layout
+const expressLayouts = require('express-ejs-layouts');
 
-//to use express router
-app.use('/',require('./routers'));
+
+// to use expresslayouts
+app.use(expressLayouts);
+
+//to extract style files for other pages
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+//to use static file
+app.use(express.static('./assets'));
 
 // to set up view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+//to use express router
+app.use('/',require('./routers'));
+
 
 app.listen(port,function(err){
     if(err){
