1b44a3b24b51b34e18478d689db47ccce0f62531
kunal swami <kunalswami29@gmail.com>
2020-06-01 19:37:33 +0530

    a user's check is added to the posts.js and home.ejs to verify user signin or not

--- routers/posts.js
+++ routers/posts.js
@@ -1,8 +1,9 @@
 const express= require('express');
 const router = express.Router();
+const passport=require('passport');
 const postsController=require('../controllers/posts_controller');//this is for accessing the models
 
 // router.get('/post',postsController.postPage);// this is after signing page rendering to the posts psge
-router.post('/create',postsController.create);//this is for calling the controller to create
+router.post('/create',passport.checkAuthentication,postsController.create);//this is for calling the controller to create
 
 module.exports=router;
