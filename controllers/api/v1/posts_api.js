5dea332156ecdff21c8b6bef298beebf85a292ed
kunal swami <kunalswami29@gmail.com>
2020-06-11 07:35:11 +0530

    a passport-jwt strategy is set up and second version to api v2 is added

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,7 +1,41 @@
-module.exports.index = function (req, res) {
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function (req, res) {
+    let posts = await Post.find({})      // this  will perform this statement first
+        .sort('-createdAt')
+        .populate('user')   // this is to populate the user i.e. to extract user whole details
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
     return res.json(200, {
         message: "List of posts",
-        posts: []
+        posts: posts
     })
+
+}
+
+module.exports.destroy = async function (req, res) {
+
+    try {
+        let post = await Post.findById(req.params.id);
+
+       
+        post.remove();
+
+        await Comment.deleteMany({ post: req.params.id });
+        res.json(200,{
+            message:'posts and assosciated content is deleted'
+        });
+        return res.redirect('back');
+    }catch(err){
+        console.log("******");
+        return res.json(500,{
+            message:'not able to dlete the post'
+        });
+    };
 
 }
--- routers/api/v1/index.js
+++ routers/api/v1/index.js
@@ -3,5 +3,4 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/posts',require('./posts'));
-
 module.exports=router;
