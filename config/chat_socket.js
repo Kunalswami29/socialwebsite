95e2fe507d43709934b8001b9a3bf84d01b8e426
kunal swami <kunalswami29@gmail.com>
2020-06-24 12:10:55 +0530

    a connection for chatroom joining is setup

--- config/chat_socket.js
+++ config/chat_socket.js
@@ -1,4 +1,23 @@
-// this is for handling the observer part for the chat
-module.exports.chatSocket = function(server){
+
+module.exports.chatSocket = function(socketServer){
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received', socket.id);
+
+        socket.on('disconnect', function(){
+            console.log('socket disconnected!');
+        });
+
+        
+        socket.on('join_room', function(data){
+            console.log('joining request rec.', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        })
+
+    });
 
 }
